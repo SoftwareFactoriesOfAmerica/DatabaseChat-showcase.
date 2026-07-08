95c9fbba39571cada939c0d12420a6863c2e55cf
Art Scott <arthur.khoury.scott@gmail.com>
2026-07-08 14:35:57 -0400

    Add files via upload

---
+++ scripts/spinner.js
@@ -0,0 +1,13 @@
+﻿function loadPage(imageUrl) {
+    const spinner = document.getElementById('loadingOverlay');
+    const heroImage = document.getElementById('heroImage');
+
+    spinner.style.display = 'flex';
+
+    heroImage.onload = function () {
+        spinner.style.display = 'none';
+    };
+
+    heroImage.src = imageUrl;
+}
+
